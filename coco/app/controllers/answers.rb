5c8f63ab539805694720a5a162031d5df45606e5
Julia Himmel & Keaty <pair+juliahimmel.keaty@devbootcamp.com>
2014-05-03 19:19:56 -0400

    viewers can now take surveys and have their answers stored in the database. if the viewer is logged in with the correct user id, they can also add questions to the survey.

--- coco/app/controllers/answers.rb
+++ coco/app/controllers/answers.rb
@@ -1,6 +1,13 @@
 post '/new_answer' do
-  option_id = Option.where(content: params[:options])[0].id
-  p params
-  Answer.create!({option_id: option_id, question_id: params[:question_id], visitor_cookie: params[:visitor_cookie]})
+  visitor_cookie = params[:visitor_cookie]
+  params.each do |key, value|
+    if key != 'visitor_cookie'
+      temp_option = Option.find(value).id
+      p temp_option
+      Answer.create!({option_id: temp_option, question_id: key, visitor_cookie: visitor_cookie})
+    end
+  end
+
   erb :thank_you
 end
+
